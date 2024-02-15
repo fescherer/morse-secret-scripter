07407ea63102f7bb79458b077867778ab8b6a50e
Felipe Scherer <felipescherer25@gmail.com>
2024-02-15 18:45:48 -0300

    :sparkles: feat: decoder encoder

--- src/app/components/encoder.tsx
+++ src/app/components/encoder.tsx
@@ -1,7 +1,24 @@
+'use client'
+
+import { useState } from 'react'
+
+const dictionary: { [code: string]: string } = {
+  'a': '.-', 'b': '-...', 'c': '-.-.', 'd': '-..', 'e': '.', 'f': '..-.', 'g': '--.', 'h': '....', 'i': '..', 'j': '.---', 'k': '-.-', 'l': '.-..', 'm': '--', 'n': '-.', 'o': '---', 'p': '.--.', 'q': '--.-', 'r': '.-.', 's': '...', 't': '-', 'u': '..-', 'v': '...-', 'w': '.--', 'x': '-..-', 'y': '-.--', 'z': '--..', ' ': '/', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----',
+}
+
 export function Encoder() {
+  const [text, setText] = useState('')
+
+  function handleEncoder(rawText: string) {
+    const parsedText = rawText.split('').map(letter => dictionary[letter.toLowerCase()])
+    console.log(parsedText, rawText)
+    setText(parsedText.join(' '))
+  }
+
   return (
-    <div>
-      <span>dsadas</span>
+    <div className="flex justify-between">
+      <textarea name="" id="" placeholder="Type here what you want to encode to morse" onChange={e => handleEncoder(e.target.value)} />
+      <textarea name="" id="" placeholder="result" value={text} disabled />
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,21 @@
+import { Decoder } from './components/decoder'
+import { Encoder } from './components/encoder'
+
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       <h1>About morse code</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis delectus aut tenetur quia, eveniet, quae culpa quibusdam porro et quas, tempora reprehenderit enim? Dolorum illo alias libero harum, perspiciatis aliquid?</p>
 
       <section>
         <h2>Morse Encoder</h2>
-        <textarea name="" id="" placeholder="Type here what you want to encode to morse" />
+        <Encoder />
       </section>
 
       <section>
         <h2>Morse Decoder</h2>
         <span>Obs. valid character= . - /</span>
-        <textarea name="" id="" placeholder="Type here what you want to decode to morse" />
+        <Decoder />
       </section>
     </div>
   )
